73831ed5f3cf15caafefb5fe8d416d82b33f88f6
unknown <T056394@cgi-114093.ad.git.cginet>
2012-08-23 12:42:24 -0400

    more test on variadic, how to forward calls from opDispatch with modified types (ie string -> char*)

--- iupSimpleDialogD/main3.d
+++ iupSimpleDialogD/main3.d
@@ -3,6 +3,8 @@ import std.stdio;
 import std.exception;
 import std.string;
 import std.conv;
+import std.typecons;
+import std.typetuple;
 
 import iup.iup;
 import iup.utild;
@@ -56,9 +58,86 @@ IupWidget Iup(string name, Args...)(Args args) {
     return new IupWidget(ih);
 }
 
+//------------------
+
+struct Toto(Specs...) {
+    template parseSpecs(Specs...) {
+        static if (Specs.length == 0)
+        {
+            alias TypeTuple!() parseSpecs;
+        }
+        else static if (is(Specs[0]))
+        {
+            static if (is(Specs[0] == string))
+            {
+                alias TypeTuple!(const(char)*,
+                                 parseSpecs!(Specs[1 .. $])) parseSpecs;
+            }
+            else
+            {
+                alias TypeTuple!(Specs[0],
+                                 parseSpecs!(Specs[1 .. $])) parseSpecs;
+            }
+        }
+        else
+        {
+            static assert(0, "Attempted to instantiate Tuple with an "
+                            ~"invalid argument: "~ Specs[0].stringof);
+        }
+    }
+
+    alias parseSpecs!Specs argsSpecs;
+}
+
+void tata(int i, char c, immutable(char)* s) {
+    writeln(i,c,s);
+}
+
+void toto(string name, Args...)(Args args) {
+    writeln(typeid(Args));
+    writeln(args[0]);
+    writeln(args[1..$]);
+    writeln();
+    auto t = tuple(args[0..2], args[2].toStringz);
+    writeln(t);
+    writeln(typeid(t));
+    tata(t.expand);
+}
+
+void titi(Args...)(Args args) {
+    alias Toto!Args TotoType;
+    TotoType toto;
+
+    writeln(typeid(toto));
+    writeln(typeid(Args));
+    writeln(typeid(toto.argsSpecs));
+}
+
 
 int main(string[] args)
 {
+    writeln("\ntoto()");
+    toto!"allo"(1,'a',"stt");
+
+    writeln("\nToto");
+    alias Toto!(int,char) TToto;
+    TToto atoto;
+    writeln(typeid(atoto));
+    writeln(typeid(atoto.argsSpecs));
+
+    writeln("\ntiti()");
+    titi('a', 10, "allo");
+
+    writeln("\nTuple");
+    alias Tuple!(int, "index", string, "value") Entry;
+    Entry e;
+    e.index = 4;
+    e.value = "Hello";
+    assert(e[1] == "Hello");
+    assert(e[0] == 4);
+    writeln(typeid(e.Types));
+
+    version(none){
     try {
         /* IUP initialization */
         IupOpenD(args);
@@ -99,6 +178,7 @@ int main(string[] args)
         IupControlsClose();
         IupClose();
     }
+    }//versionnone
 
 	return 0;
 }
