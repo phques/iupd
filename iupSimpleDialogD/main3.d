ceedc80ef05385923aa414c0f9865ed1a287cda1
Philippe Quesnel <philippe.quesnel@gmail.com>
2012-08-23 22:26:11 -0400

    removed tests that are now part of main4.d

--- iupSimpleDialogD/main3.d
+++ iupSimpleDialogD/main3.d
@@ -60,84 +60,11 @@ IupWidget Iup(string name, Args...)(Args args) {
 
 //------------------
 
-struct Toto(Specs...) {
-    template parseSpecs(Specs...) {
-        static if (Specs.length == 0)
-        {
-            alias TypeTuple!() parseSpecs;
-        }
-        else static if (is(Specs[0]))
-        {
-            static if (is(Specs[0] == string))
-            {
-                alias TypeTuple!(const(char)*,
-                                 parseSpecs!(Specs[1 .. $])) parseSpecs;
-            }
-            else
-            {
-                alias TypeTuple!(Specs[0],
-                                 parseSpecs!(Specs[1 .. $])) parseSpecs;
-            }
-        }
-        else
-        {
-            static assert(0, "Attempted to instantiate Tuple with an "
-                            ~"invalid argument: "~ Specs[0].stringof);
-        }
-    }
-
-    alias parseSpecs!Specs argsSpecs;
-}
-
-void tata(int i, char c, immutable(char)* s) {
-    writeln(i,c,s);
-}
-
-void toto(string name, Args...)(Args args) {
-    writeln(typeid(Args));
-    writeln(args[0]);
-    writeln(args[1..$]);
-    writeln();
-    auto t = tuple(args[0..2], args[2].toStringz);
-    writeln(t);
-    writeln(typeid(t));
-    tata(t.expand);
-}
-
-void titi(Args...)(Args args) {
-    alias Toto!Args TotoType;
-    TotoType toto;
-
-    writeln(typeid(toto));
-    writeln(typeid(Args));
-    writeln(typeid(toto.argsSpecs));
-}
 
 
 int main(string[] args)
 {
-    writeln("\ntoto()");
-    toto!"allo"(1,'a',"stt");
 
-    writeln("\nToto");
-    alias Toto!(int,char) TToto;
-    TToto atoto;
-    writeln(typeid(atoto));
-    writeln(typeid(atoto.argsSpecs));
-
-    writeln("\ntiti()");
-    titi('a', 10, "allo");
-
-    writeln("\nTuple");
-    alias Tuple!(int, "index", string, "value") Entry;
-    Entry e;
-    e.index = 4;
-    e.value = "Hello";
-    assert(e[1] == "Hello");
-    assert(e[0] == 4);
-    writeln(typeid(e.Types));
-
-    version(none){
     try {
         /* IUP initialization */
         IupOpenD(args);
@@ -178,7 +105,7 @@ int main(string[] args)
         IupControlsClose();
         IupClose();
     }
-    }//versionnone
+
 
 	return 0;
 }
